e6ab1272f4ada81eac8e06f717222ffa1f57bd1e
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-06 15:06:58 +0700

    Add sign up route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const user_controller = require("../controllers/userController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+router.get("/sign-up", user_controller.user_signup_get);
 
 module.exports = router;
