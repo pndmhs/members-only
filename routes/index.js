6c80ab153f71bb71d8d0c07f3cd6c8b64240cc73
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-07 07:09:15 +0700

    Add login get route

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,6 @@ router.get("/sign-up", user_controller.user_signup_get);
 
 router.post("/sign-up", user_controller.user_signup_post);
 
+router.get("/login", user_controller.user_login_get);
+
 module.exports = router;
