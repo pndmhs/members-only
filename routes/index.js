dba33c3d96d517e7487cfb0d375cbf2ff0ea4576
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-07 11:15:06 +0700

    Change home router with controller function

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const user_controller = require("../controllers/userController");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/", user_controller.home_get);
 
 router.get("/sign-up", user_controller.user_signup_get);
 
