3502648c43c3f38370f9966f2ad9fcfb85ac14c9
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-02-08 21:46:04 +0700

    Fix fullname get function

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
   },
 });
 
-UserSchema.virtual("fullName").get(() => {
+UserSchema.virtual("fullName").get(function () {
   return this.firstName + " " + this.lastName;
 });
 
